09d71587abe08dbe3a7aa179370c83faf3fed128
Alex Sinishin <sinishin@gmail.com>
2013-11-22 02:09:05 -0800

    Spelling

---
+++ db/migrate/20131121173237_create_customer_deltas.rb
@@ -0,0 +1,16 @@
+class CreateCustomerDeltas < ActiveRecord::Migration
+  def change
+    create_table :customer_deltas do |t|
+      t.string  :operation,     length: 5, null: false
+      t.decimal :input_order,   null: false, :precision => 15, :scale => 0
+      t.string  :status,        length: 5, null: false, default: 'wait'
+
+      t.string  :first_name
+      t.string  :last_name
+
+      t.integer :customer_ref_id, null: false
+
+      t.timestamps
+    end
+  end
+end
