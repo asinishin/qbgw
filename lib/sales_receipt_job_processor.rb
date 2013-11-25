5a2a763ccda15a979708e6d0a8e729fef72e3531
Alex Sinishin <sinishin@gmail.com>
2013-11-25 07:03:02 -0800

    Implemented deletion

--- lib/sales_receipt_job_processor.rb
+++ lib/sales_receipt_job_processor.rb
@@ -14,15 +14,14 @@ class SalesReceiptJobProcessor
 	dels << delta
       end
 
-      if false # dels.size > 0
+      if dels.size > 0
 	request = [{ 
 	  :xml_attributes =>  { "onError" => "stopOnError" }, 
 	  :txn_del_rq => dels.map do |delta|
 	    {
 	      :xml_attributes => { "requestID" => delta.id },
-	      :txn_del => {
-		:list_id => delta.sales_receipt_ref.qb_id
-	      }
+	      :txn_del_type   => "SalesReceipt",
+	      :txn_id => delta.sales_receipt_ref.qb_id
 	    }
 	  end 
 	}]
@@ -128,13 +127,13 @@ class SalesReceiptJobProcessor
   def self.process_response(r)
     r = r['qbxml_msgs_rs'] if r['qbxml_msgs_rs']
 
-    # ItemServiceModRs array case # TODO REMOVAL should be here
-#    if r['item_service_mod_rs'].respond_to?(:to_ary)
-#      r['item_service_mod_rs'].each{ |item| ItemServiceJobProcessor::process_response_item item }
-#    # Or one item
-#    elsif r['item_service_mod_rs']
-#      ItemServiceJobProcessor::process_response_item r['item_service_mod_rs']
-#    end
+    # TxnDelRs array case 
+    if r['txn_del_rs'].respond_to?(:to_ary)
+      r['txn_del_rs'].each{ |item| SalesReceiptJobProcessor::process_response_item item }
+    # Or one item
+    elsif r['txn_del_rs']
+      SalesReceiptJobProcessor::process_response_item r['txn_del_rs']
+    end
 
     # SalesReceiptAddRs array case
     if r['sales_receipt_add_rs'].respond_to?(:to_ary)
