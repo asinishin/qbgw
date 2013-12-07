961bfe89b5ece161a195dffa4516926ce2fa0525
Alex Sinishin <sinishin@gmail.com>
2013-12-07 02:23:22 -0800

    Fixed attr access

--- app/models/sales_receipt_line.rb
+++ app/models/sales_receipt_line.rb
@@ -1,5 +1,6 @@
 class SalesReceiptLine < ActiveRecord::Base
-  attr_accessible :amount, :class_ref, :item_id, :quantity, :sales_receipt_bit_id
+  attr_accessible :amount, :class_ref, :item_id, :quantity,
+                  :sales_receipt_bit_id, :txn_line_id
 
   belongs_to :sales_receipt_bit
 end
