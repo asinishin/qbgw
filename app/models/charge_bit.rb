0a29dc27f6fcfa0624f72ef972b671375e4bef97
Alex Sinishin <sinishin@gmail.com>
2013-12-08 21:21:58 -0800

    Fixed class ref attribute

--- app/models/charge_bit.rb
+++ app/models/charge_bit.rb
@@ -1,4 +1,4 @@
 class ChargeBit < ActiveRecord::Base
-  attr_accessible :amount, :charge_ref_id, :customer_id, :item_id, :operation,
+  attr_accessible :amount, :charge_ref_id, :class_ref, :customer_id, :item_id, :operation,
                   :quantity, :ref_number, :status, :txn_date
 end
