a77d9b6e5e2527a9fd28d4a580ddeced3b7cbd26
Alex Sinishin <sinishin@gmail.com>
2013-12-10 12:14:53 -0800

    Added accessor

--- app/models/sales_receipt_bit.rb
+++ app/models/sales_receipt_bit.rb
@@ -1,5 +1,6 @@
 class SalesReceiptBit < ActiveRecord::Base
-  attr_accessible :customer_id, :operation, :ref_number, :sales_receipt_ref_id, :status, :txn_date
+  attr_accessible :account_ref, :customer_id, :operation, :ref_number, :sales_receipt_ref_id,
+                  :status, :txn_date
 
   belongs_to :sales_receipt_ref
   has_many   :sales_receipt_lines
