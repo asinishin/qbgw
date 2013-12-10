aca4ad1d126caef536d6d1a3dc72d3215e322afd
Alex Sinishin <sinishin@gmail.com>
2013-12-10 13:52:48 -0800

    Fixed accessor

--- app/models/st_purchase.rb
+++ app/models/st_purchase.rb
@@ -1,4 +1,4 @@
 class StPurchase < ActiveRecord::Base
-  attr_accessible :qb_customer_list_id, :is_cashed, :ref_number,
+  attr_accessible :account_ref, :qb_customer_list_id, :is_cashed, :ref_number,
                   :sat_customer_id, :sat_id, :txn_date
 end
